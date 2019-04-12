b9a4fe5fb2d6c9a38b9ad3ef2334c653a942f16b
makeitokay <andrey.mu11@yandex.ru>
2019-04-12 23:16:05 +0500

    Подправил структуру

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from okayenglish.bot import Bot
-from flask import Flask, request
-
-app = Flask(__name__)
-bot = Bot(app)
-
-
-@app.route("/post", methods=["POST"])
-def get_request():
-    return bot.handle_request(request.json)
-
+from okayenglish.app import app
+from okayenglish.db import *
 
 if __name__ == "__main__":
     app.run()
---
+++ okayenglish/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+from .config import *
+
+from okayenglish.bot import Bot
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = TRACK_MODIFICATIONS
+db = SQLAlchemy(app)
+bot = Bot(app)
+
+
+@app.route("/post", methods=["POST"])
+def get_request():
+    return bot.handle_request(request.json)
