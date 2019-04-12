4f6a9515382cf5384da1a72aaadfe4647e032b77
makeitokay <andrey.mu11@yandex.ru>
2019-04-12 23:35:09 +0500

    Классы RequestParser, ResponseParser
    Реализован простой эхо-бот

--- okayenglish/bot.py
+++ okayenglish/bot.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import json
 
+from .res_req_parser import RequestParser, ResponseParser
+
 
 class Bot:
     """Ядро навыка для приложения ``app``"""
@@ -8,25 +10,10 @@ class Bot:
     def __init__(self, app: Flask):
         self.app = app
 
-    def handle_request(self, req):
-        return json.dumps({
-            "response": {
-                "text": "Здравствуйте! Это мы, хороводоведы.",
-                "tts": "Здравствуйте! Это мы, хоров+одо в+еды.",
-                "buttons": [
-                    {
-                        "title": "Надпись на кнопке",
-                        "payload": {},
-                        "url": "https://example.com/",
-                        "hide": True
-                    }
-                ],
-                "end_session": False
-            },
-            "session": {
-                "session_id": "2eac4854-fce721f3-b845abba-20d60",
-                "message_id": 4,
-                "user_id": "AC9WC3DF6FCE052E45A4566A48E6B7193774B84814CE49A922E163B8B29881DC"
-            },
-            "version": "1.0"
-        })
+    def handle_request(self, req_json):
+        req = RequestParser(req_json)
+        response = ResponseParser(req)
+
+        # Простой эхо-бот.
+        response.set_reply_text(req.get_text())
+        return json.dumps(response)
---
+++ okayenglish/res_req_parser.py
@@ -0,0 +1,34 @@
+TYPICAL_RESPONSE = {
+    'session': None,
+    'version': None,
+    'response': {
+        'end_session': False
+    }
+}
+
+
+class RequestParser(dict):
+    def __init__(self, request_json):
+        super().__init__(request_json)
+
+    def get_session(self):
+        return self.get("session")
+
+    def get_version(self):
+        return self.get("version")
+
+    def get_text(self):
+        return self.get("request").get("original_utterance")
+
+
+class ResponseParser(dict):
+    def __init__(self, request: RequestParser):
+        super().__init__()
+        for k, v in TYPICAL_RESPONSE.items():
+            if k in ("session", "version"):
+                self[k] = request.get(k)
+            else:
+                self[k] = v
+
+    def set_reply_text(self, text):
+        self["response"]["text"] = text
